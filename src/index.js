2b2472b7b29710a90381ba13879f5d3b9294c932
noir210397 <77852592+noir210397@users.noreply.github.com>
2023-06-02 14:55:39 +0100

    final

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/zoopla'>
+    <HashRouter >
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
